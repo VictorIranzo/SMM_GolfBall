3d2ebd7821522c4bb5601a1f1eb1dbbd5336e28a
Víctor Iranzo <viranzo96@gmail.com>
2018-02-02 19:53:22 +0100

    First Commit
    Unit the second video.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody playerRigidBody;
+
+    void Start() {
+        // Gets the Rigidbody component of the player.
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+	// Called before frame is rendered.
+	void Update () {
+		
+	}
+
+	// Called before each physics compute.
+	void FixedUpdate () {
+        // Record the input from the keybord for the axis.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        playerRigidBody.AddForce(movement * speed);
+	}
+}
