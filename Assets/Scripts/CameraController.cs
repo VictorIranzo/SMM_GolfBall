7b17a28274a8ba74641aff4c7580604cafa9cb4f
VictorIranzo <viranzo96@gmail.com>
2018-02-05 01:25:37 +0100

    Camera now follows the ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Runs for every frame, but when we update position of the camera we ensure that the player has moved to that frame.
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    // This property is set from the Unity enviroment.
     public float speed;
 
     private Rigidbody playerRigidBody;
 
+    // Reference to Rigidbody is set in this method. It is called exactly once in the lifetime of the script before any of the Update methods.
     void Start() {
         // Gets the Rigidbody component of the player.
         playerRigidBody = GetComponent<Rigidbody>();
@@ -24,8 +26,10 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        // It doesn't move up (in the y axis).
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+        // The vector is multiply by the speed scalar.
         playerRigidBody.AddForce(movement * speed);
 	}
 }
