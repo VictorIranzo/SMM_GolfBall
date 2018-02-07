1a49975537705b093e97dba225c5d66efb4f5ead
VictorIranzo <viranzo96@gmail.com>
2018-02-07 23:47:43 +0100

    Added points count and  win message.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     // This property is set from the Unity enviroment.
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody playerRigidBody;
     private int pointsCount;
@@ -15,6 +18,8 @@ public class PlayerController : MonoBehaviour {
         // Gets the Rigidbody component of the player.
         playerRigidBody = GetComponent<Rigidbody>();
         pointsCount = 0;
+        this.SetCountText();
+        winText.text = "";
     }
 
 	// Called before frame is rendered.
@@ -36,13 +41,23 @@ public class PlayerController : MonoBehaviour {
 	}
 
     // other is the object which we have collide.
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             pointsCount++;
+            this.SetCountText();
         }
         // Destroy(other.gameObject); This sentence destroy the objects.
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + pointsCount.ToString();
+        if (pointsCount >= 8)
+        {
+            winText.text = "You win!";
+        }
     }
 }
