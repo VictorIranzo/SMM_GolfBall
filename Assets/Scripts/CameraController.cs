76fd67477fbc75208472a0881688d2c7f774e5ab
Víctor Iranzo <viranzo96@gmail.com>
2018-02-09 17:57:32 +0100

    Changed camera to move it more smoothly.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;
-
+    public Transform player;
     private Vector3 offset;
+    public float speed = 0.1f;
+    private static float t = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-        offset = transform.position - player.transform.position;
-	}
+    private Vector3 To, From;
 	
 	// Runs for every frame, but when we update position of the camera we ensure that the player has moved to that frame.
-	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+	void Update () {
+        To = player.position;
+        From = transform.position;
+        transform.position = new Vector3(Mathf.Lerp(From.x, To.x, t),0, Mathf.Lerp(From.z, To.z, t));
+
+        t += speed * Time.deltaTime;
+        float dist = Vector3.Distance(player.position, transform.position);
+
+        if (dist < 0.2f) t = 0f;
+    }
 }
