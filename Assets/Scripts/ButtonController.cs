e750dcab0dd4ced0742a4cbc14cec92d88e492b4
Víctor Iranzo <viranzo96@gmail.com>
2018-02-16 18:00:24 +0100

    Added first animations.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.UIElements;
+
+public class ButtonController : MonoBehaviour, IPointerClickHandler { 
+    public Animator animator;
+    private int counter = 0;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        counter++;
+        if (counter == 10)
+        {
+            counter = 0;
+            animator.SetTrigger("Live");
+        }
+    }
+}
