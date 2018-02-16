b92035665667b6bc30990a3298e5c3d0bc963422
Víctor Iranzo <viranzo96@gmail.com>
2018-02-16 18:21:13 +0100

    Modifying controller.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour, IPointerClickHandler { 
     public Animator animator;
+    public Text liveText;
     private int counter = 0;
 
     public void OnPointerClick(PointerEventData eventData)
@@ -15,6 +17,11 @@ public class ButtonController : MonoBehaviour, IPointerClickHandler {
         {
             counter = 0;
             animator.SetTrigger("Live");
+        }
+        int lives;
+        int.TryParse(liveText.text, out lives);
+        if (lives>0) {
+            liveText.text = (lives - 1).ToString();
         }
     }
 }
