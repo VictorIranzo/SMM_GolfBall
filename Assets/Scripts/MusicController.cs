5c47a53364c28448e856ee3352d17d6ee7f3d641
VictorIranzo <viranzo96@gmail.com>
2018-02-18 00:48:34 +0100

    Added music controller.

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour {
+
+    public Image soundImage;
+
+    private Animator animator;
+    private AudioSource music;
+
+    private bool isMuted;
+
+	// Use this for initialization
+	void Start () {
+        music = GetComponent<AudioSource>();
+        animator = soundImage.GetComponent<Animator>();
+        isMuted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMusicState() {
+        if (!isMuted)
+        {
+            Debug.Log("Click");
+            music.Pause();
+            isMuted = true;
+            animator.SetBool("mute",true);
+        }
+        else {
+            music.UnPause();
+            isMuted = false;
+            animator.SetTrigger("ActiveSound");
+            animator.SetBool("mute", false);
+        }
+    }
+}
